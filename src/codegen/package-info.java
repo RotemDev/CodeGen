6e750a26c304956deeca065bd73aa7ae6b6a1da4
RoteMinecraftIL <rotem.moses@outlook.co.il>
2016-07-08 14:19:52 +0300

    Commit #1
    Created package-info files

---
+++ src/codegen/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * <b><i>CodeGen</i></b> is a code generating program. The user signs up/logs in,
+ * and the program generates codes for him. <br>
+ * <p><b><i>NOTE:</i></b> The program does NOT generate any Game/Coupon codes, but you can try.</p>
+ * @author RotemDev
+ * @version 1.0
+ * @since July 2016
+ * @category Code Generators
+ * @see codegen.res
+ */
+package codegen;
---
+++ src/codegen/res/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * <i>codegen.res</i> is the non-code resources (such as images, text files, etc.)
+ * @author RotemDev
+ * @see codegen
+ */
+package codegen.res;
